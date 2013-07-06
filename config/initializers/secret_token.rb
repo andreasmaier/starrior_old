772e36c8ba9079a6c246fc09a889663ffc72b14b
Andreas Maier <andreas.maier@gmail.com>
2013-07-06 13:41:34 -0700

    rails initialised

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starrior::Application.config.secret_token = '74910ca382c1acf7c58fccd5d99d3de2f56f5b1ce0a417785fdafb1ba6d9859cf59aaf98e098f505209f348249cbad9127170df92dbcd11e016ef3cd6588c961'
