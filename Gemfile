eee3f20a71197336b8fd00a6c2cf138eedda6412
Andreas Maier <andreas.maier@gmail.com>
2013-07-06 16:13:37 -0700

    switch to rspec, first tests

--- Gemfile
+++ Gemfile
@@ -3,8 +3,9 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 
-group :development do
+group :development, :test do
   gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails', '2.13.1'
 end
 
 group :test do
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Starrior::Application.config.secret_token = '74910ca382c1acf7c58fccd5d99d3de2f56f5b1ce0a417785fdafb1ba6d9859cf59aaf98e098f505209f348249cbad9127170df92dbcd11e016ef3cd6588c961'
+Starrior::Application.config.secret_key_base = '371c96e1c389e03ea744303aa605025242d13529b48283cfea933253b0714712e34938818687323ecfbeb0f25cfc3df87c5d6009487eb1d5c7a15c4e70b0b0ca'
