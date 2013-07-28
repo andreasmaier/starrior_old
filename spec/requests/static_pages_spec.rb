852efe772aa9dbc9dfa2c93b1276bcab8df91ab1
Andreas Maier <andreas.maier@gmail.com>
2013-07-27 23:42:52 -0700

    commit before nuking

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,38 +2,50 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home Page" do
-    it "should have the content 'Starrior'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Starrior')
+    it "should have the h1 'StarrioR'" do
+      visit root_path
+      expect(page).to have_content('StarrioR')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Starrior | Home")
+      visit root_path
+      expect(page).to have_title("StarrioR | Home")
     end
   end
 
   describe "Help Page" do
-    it "should have the content 'Starrior'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Starrior')
+    it "should have the content 'StarrioR'" do
+      visit help_path
+      expect(page).to have_content('StarrioR')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Starrior | Help")
+      visit help_path
+      expect(page).to have_title("StarrioR | Help")
     end
   end
 
   describe "About Page" do
-    it "should have the content 'Starrior'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('Starrior')
+    it "should have the content 'StarrioR'" do
+      visit about_path
+      expect(page).to have_content('StarrioR')
     end
 
     it "should have the title 'About'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Starrior | About")
+      visit about_path
+      expect(page).to have_title("StarrioR | About")
+    end
+  end
+
+  describe "Contact page" do
+    it "shuold have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("StarrioR | Contact")
     end
   end
 end
